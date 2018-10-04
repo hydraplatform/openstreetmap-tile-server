bf8ad953ac18e3b25ea4becc2637f896f557168f
Giovanni Basolu <giovanni.basolu@gmail.com>
2018-10-04 15:11:58 +0100

    added the base path

--- variables.sh
+++ variables.sh
@@ -6,3 +6,4 @@ osm_step_2_image_name="traning-hydra-osm-tile-server-finale"
 osm_container_name="hydra-osm-tile-server-container"
 osm_data_volume="openstreetmap-data"
 osm_tiles_volume="openstreetmap-rendered-tiles"
+osm_root_folder=$(cd && pwd)
