673fb2ab798cc03bbf83e9b748359bc5c06d982c
Giovanni Basolu <giovanni.basolu@gmail.com>
2018-10-04 15:52:55 +0100

    added the correct basepath and added the rm option to run

--- import-data.sh
+++ import-data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+source ./variables.sh
 filename=$1
 # echo $filename
 
-filepath="/home/training/data/$filename.osm.pbf"
+filepath="$osm_root_folder/data/$filename-latest.osm.pbf"
 # echo $filepath
 if [[ -e $filepath ]]; then
 	echo "Importing $filepath"
-   	docker run -v $filepath:/data.osm.pbf -v $osm_data_volume:/var/lib/postgresql/10/main $osm_step_2_image_name import
+   	docker run --rm -v $filepath:/data.osm.pbf -v $osm_data_volume:/var/lib/postgresql/10/main $osm_step_2_image_name import
 else
 	echo "$filepath Not Found!"
 fi
