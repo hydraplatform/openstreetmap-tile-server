614dc51e46336b99883cc242e8a9fe08305ca183
Giovanni Basolu <giovanni.basolu@gmail.com>
2018-10-05 12:29:11 +0100

    now the port of osm server is 8190

--- run-server.sh
+++ run-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./variables.sh
 echo "Running the Tile server"
-docker run --rm -p 80:80 --name $osm_container_name \
+docker run --rm -p 8190:80 --name $osm_container_name \
                     -v $osm_data_volume:/var/lib/postgresql/10/main \
                     -v $osm_tiles_volume:/var/lib/mod_tile -d \
                     $osm_step_2_image_name run # overv/openstreetmap-tile-server run
