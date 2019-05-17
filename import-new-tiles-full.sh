e1610f1c45a53fc296a92e573ff370e0e0b83063
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-05-17 18:36:39 +0100

    logs

--- import-new-tiles-full.sh
+++ import-new-tiles-full.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+source ./variables.sh
 filename=$1
 now=`date +"%Y-%m-%d.%H.%M.%S"`
-mkdir -p "$osm_root_folder/logs"
-./stop-and-remove-container.sh > "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
-./remove-volumes.sh >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
-./setup-server.sh >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
-./import-data.sh filename >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
-./run-server.sh >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
+mkdir -p "$osm_root_folder/logs/$filename"
+./stop-and-remove-container.sh > "$osm_root_folder/logs/import.$now.log" 2>&1
+./remove-volumes.sh >> "$osm_root_folder/logs/import.$now.log" 2>&1
+./setup-server.sh >> "$osm_root_folder/logs/import.$now.log" 2>&1
+./import-data.sh filename >> "$osm_root_folder/logs/import.$now.log" 2>&1
+./run-server.sh >> "$osm_root_folder/logs/import.$now.log" 2>&1
