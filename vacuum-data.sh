32f461ff7c91bf13942300841fef7f59e4577b4a
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-05-17 16:49:14 +0100

    bf

--- vacuum-data.sh
+++ vacuum-data.sh
@@ -2,5 +2,4 @@
 source ./variables.sh
 
 echo "Deleting all DATA"
-docker run --rm -v $osm_data_volume:/var/lib/postgresql/10/main
-								$osm_step_2_image_name vacuum
+docker run --rm -v $osm_data_volume:/var/lib/postgresql/10/main $osm_step_2_image_name vacuum
