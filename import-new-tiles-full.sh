e6eab93e111fca3792655600f4615610d2ada4f4
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-05-17 18:32:36 +0100

    logs

--- import-new-tiles-full.sh
+++ import-new-tiles-full.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 filename=$1
-
-./stop-and-remove-container.sh
-./remove-volumes.sh
-./setup-server.sh
-./import-data.sh filename
-./run-server.sh
+now=`date +"%Y-%m-%d.%H.%M.%S"`
+mkdir -p "$osm_root_folder/logs"
+./stop-and-remove-container.sh > "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
+./remove-volumes.sh >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
+./setup-server.sh >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
+./import-data.sh filename >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
+./run-server.sh >> "$osm_root_folder/logs/import.$filename.$now.log" 2>&1
